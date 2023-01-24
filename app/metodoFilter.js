4db2302dcbf08ba90710699fd287f60b4d9b31d9
SAMUEL BERNARDO <108311581+Smkhb@users.noreply.github.com>
2023-01-24 12:06:47 -0300

    Aula: Manipulando HTML  com JS

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,8 +4,31 @@ botoes.forEach(btn=>btn.addEventListener('click',filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
+
     let livrosFiltrados = categoria=='disponivel'
-    ?livros.filter(livro=>livro.quantidade>0)
-    :livros.filter(livro=>livro.categoria == categoria)
-    exibirOsLivrosNaTela(livrosFiltrados)
+    ?filtrarPorDisponibilidade()
+    :filtrarPorCategoria(categoria);
+
+    exibirOsLivrosNaTela(livrosFiltrados);
+    if (categoria=='disponivel') {
+        exibirValorTotalDosLivrosDisponovieisNaTela();
+    }
+
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponovieisNaTela(){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = 
+    `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        </div>
+    `
 }
